c5bde0e7575eedd0a164548acf48bc076a343d19
Philip <philipmjobe@gmail.com>
2021-11-30 13:01:46 -0500

    added employeeform.vue and updated app.vue

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,39 @@
   <div id="app" class="small-container">
     <h1>Employees</h1>
 
-    <employee-table />
+    <employee-table :employees="employees" />
   </div>
 </template>
 
 <script>
 import EmployeeTable from "@/components/EmployeeTable.vue";
+import EmployeeForm from "@/components/EmployeeForm.vue";
 
 export default {
   name: "app",
   components: {
     EmployeeTable,
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: "Richard Hendricks",
+          email: "richard@piedpiper.com",
+        },
+        {
+          id: 2,
+          name: "Bertram Gilfoyle",
+          email: "gilfoyle@piedpiper.com",
+        },
+        {
+          id: 3,
+          name: "Dinesh Chugtai",
+          email: "dinesh@piedpiper.com",
+        },
+      ],
+    };
   },
 };
 </script>
